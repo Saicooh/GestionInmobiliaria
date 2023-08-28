e3a6531a15bf76eb2f262bde7a9716872f2ab088
José <jose.lara.a01@mail.pucv.cl>
2023-08-27 20:27:18 -0400

    Primer commit

---
+++ src/GestionInmobiliaria.java
@@ -0,0 +1,9 @@
+import java.io.*;
+
+public class GestionInmoliaria
+{
+    public static void main(String args[]) throws IOException
+    {
+        System.out.println("Bienvenido a la Gestion Inmobilaria");
+    }
+}
