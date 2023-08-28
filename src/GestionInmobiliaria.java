ce382bac54836eb21c3413e0dd15d1b5781fd5e8
Claudio Toledo <claudio.toledo.m@mail.pucv.cl>
2023-08-27 20:48:25 -0400

    Segundo Commit

--- src/GestionInmobiliaria.java
+++ src/GestionInmobiliaria.java
@@ -1,9 +1,10 @@
 import java.io.*;
 
-public class GestionInmoliaria
+public class GestionInmobiliaria
 {
     public static void main(String args[]) throws IOException
     {
         System.out.println("Bienvenido a la Gestion Inmobilaria");
+        System.out.println("ola");
     }
 }
