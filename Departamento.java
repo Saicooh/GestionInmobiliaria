a8c836d75aa4b4481df8c22e81368b4c591ceb62
Claudio Toledo <claudio.toledo.m@mail.pucv.cl>
2023-08-27 23:05:47 -0400

    Se ha agregado Clase Departamento

---
+++ Departamento.java
@@ -0,0 +1,47 @@
+public class Departamento
+{
+    private int numero;
+    private int cantidadHabitaciones;
+    private boolean disponible;
+    private Tipo tipo;
+
+    public Departamento(int numero, int cantidadHabitaciones, boolean disponible, String nombreTipo, int precio)
+    {
+        this.numero = numero;
+        this.cantidadHabitaciones = cantidadHabitaciones;
+        this.disponible = true;
+        this.tipo = new Tipo(nombreTipo, precio);
+    }
+
+    public int getNumero()
+    {
+        return this.numero;
+    }
+
+    public int getCantidadHabitaciones()
+    {
+        return this.cantidadHabitaciones;
+    }
+
+    public boolean getDisponible()
+    {
+        return this.disponible;
+    }
+
+    public void setNumero(int numero)
+    {
+        this.numero = numero;
+    }
+
+    public void setCantidadHabitaciones(int cantidadHabitaciones)
+    {
+        this.cantidadHabitaciones = cantidadHabitaciones;
+    }
+
+    public void getTipo()
+    {
+        this.tipo.getNombre();
+        this.tipo.getPrecio();
+    }
+
+}
