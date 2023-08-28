1773a717d41cfc2451ba25a3551eb2fdfed249a4
Fabian Solis <fabian.solis.p@mail.pucv.cl>
2023-08-27 22:39:55 -0400

    metodos modularizados

---
+++ Sistema.java
@@ -0,0 +1,48 @@
+public class Sistema
+{
+    public void agregarEdificio()
+    {
+
+    }
+
+    public void buscarEdificio()
+    {
+
+    }
+
+    public void eliminarEdificio()
+    {
+
+    }
+
+    public void mostrarEdificios()
+    {
+
+    }
+
+    public void agregarDepartamento()
+    {
+
+    }
+
+    public void buscarDepartamento()
+    {
+
+    }
+
+    public void eliminarDepartamento()
+    {
+
+    }
+
+    public void mostrarDepartamentos()
+    {
+
+    }
+
+    public void mostrarDepartamentosDisponibles()
+    {
+
+    }
+}
+
