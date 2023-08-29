4df3e488b72f2f4919b016716b5d4b8e7bd47c5c
José <jose.lara.a01@mail.pucv.cl>
2023-08-29 01:02:24 -0400

    Prototipados métodos de Sistema.java

--- Sistema.java
+++ Sistema.java
@@ -1,48 +1,95 @@
-
 public class Sistema
 {
     public void agregarEdificio()
     {
+        // Pedir nombre y dirección
 
+        // Crear edificio
+
+        // Agregar edificio al mapa
+
+        // Mostrar mensaje de éxito
     }
 
     public void buscarEdificio()
     {
+        // Pedir nombre de edificio
 
+        // Buscar edificio en el mapa
+
+        // Mostrar edificio y mostrar menu de opciones
+
+        // Si no existe, mostrar mensaje de error
+
+        // Si existe, mostrar menu de opciones
     }
 
     public void eliminarEdificio()
     {
+        // Pedir nombre de edificio
 
+        // Buscar edificio en el mapa
+
+        // Si no existe, mostrar mensaje de error
+
+        // Si existe, eliminar edificio del mapa y mostrar mensaje de éxito
     }
 
-    public void mostrarEdificios()
+    public void mostrarEdificios(boolean disponible)
     {
+        // Mostrar todos los edificios del mapa
 
+        // Si no hay edificios, mostrar mensaje de error
     }
 
     public void agregarDepartamento()
     {
+        // Pedir tipo de departamento
 
+        // Pedir cantidad de habitaciones
+
+        // Pedir precio
+
+        // Crear departamento
+
+        // Agregar departamento al edificio
+
+        // Mostrar mensaje de éxito
     }
 
     public void buscarDepartamento()
     {
+        // Pedir número de departamento
 
+        // Buscar departamento en el edificio
+
+        // Si no existe, mostrar mensaje de error
+
+        // Si existe, mostrar departamento y mostrar menu de opciones
     }
 
     public void eliminarDepartamento()
     {
+        // Pedir número de departamento
 
+        // Buscar departamento en el edificio
+
+        // Si no existe, mostrar mensaje de error
+
+        // Si existe, eliminar departamento del edificio y mostrar mensaje de éxito
     }
 
     public void mostrarDepartamentos()
     {
+        // Mostrar todos los departamentos del edificio, con su tipo, precio y disponibilidad
 
+        // Si no hay departamentos, mostrar mensaje de error
     }
 
     public void mostrarDepartamentosDisponibles()
     {
+        // Mostrar todos los departamentos disponibles del edificio, con su tipo y precio
 
+        // Si no hay departamentos disponibles, mostrar mensaje de que no hay departamentos disponibles
     }
 }
